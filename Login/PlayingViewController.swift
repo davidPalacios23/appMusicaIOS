a3c181f154a2b5bde1226421d8fe419cac36fef5
alumnos <alumnos@516-05.local>
2017-11-24 20:40:09 +0100

    efecto visual de fondo y efecto de cambio de canción con sweipt

--- Login/PlayingViewController.swift
+++ Login/PlayingViewController.swift
@@ -10,14 +10,45 @@ class PlayingViewController: UIViewController {
     
     @IBOutlet weak var imageSong: UIImageView!
     
+    @IBOutlet weak var background: UIImageView!
+    
+    var currentSong: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-      nameSong.text = songsList[0].name
-      artistSong.text = songsList[0].artist
-      imageSong.image = songsList[0].image
+      currentSong = 0
+        
+      newSong(songIndex: 0)
     }
 
 
+    @IBAction func changeRight(_ sender: Any) {
+        
+        if currentSong < songsList.count-1 {
+            currentSong += 1
+            newSong(songIndex: currentSong)
+            
+        }
+       
+    }
 
+    @IBAction func changeLeft(_ sender: Any) {
+        
+        if currentSong > 0 {
+            currentSong -= 1
+            newSong(songIndex: currentSong)
+        }
+        
+    }
+   
+   
+  
+    
+    func newSong(songIndex: Int) {
+        nameSong.text = songsList[songIndex].name
+        artistSong.text = songsList[songIndex].artist
+        imageSong.image = songsList[songIndex].image
+        background.image = songsList[songIndex].image
+    }
 }
